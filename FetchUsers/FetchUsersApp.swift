ce393b4cd5e332bbf5aeecc42ebc2a022f9f26bf
LuiArm <Luisarmendariz24@icloud.com>
2023-07-25 23:06:14 -0600

    Initial Commit

---
+++ FetchUsers/FetchUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchUsersApp.swift
+//  FetchUsers
+//
+//  Created by luis armendariz on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
